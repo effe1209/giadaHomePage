b31a346ffb5c6fc3c4cd4e4cab683e5e55539d5b
f.fiaschi6 <f.fiaschi6@studenti.unipi.it>
2025-06-13 16:12:36 +0200

    remove markers

--- scripts/main.js
+++ scripts/main.js
@@ -56,7 +56,7 @@ ScrollTrigger.matchMedia({
           start: "top top+=100",
           end: "bottom top+=600",
           scrub: true,
-          markers: true, // per debug
+          markers: false, // per debug
           onUpdate: (self) => {
             const progress = self.progress;
             const threshold = i * 0.15;
